0d8ec80970ef81c17acfc1f25874152b1fcb6c01
sharang.gupta@sitpune.edu.in <sharang.gupta@sitpune.edu.in>
2021-01-02 01:43:03 +0000

    added cast vote and check vote functionality to api

--- voting/urls.py
+++ voting/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('test', views.some_view, name="some_view"),
+    path('vote', views.cast_vote, name="cast_vote"),
+    path('check', views.check_vote, name="check_vote"),
+    
 ]
