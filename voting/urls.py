881a0a65c60f4777efeedbb6e543744a44accb8c
Sharang Gupta <sharang.gupta@sitpune.edu.in>
2021-01-04 14:06:37 +0000

    Update urls.py
    separated api endpoints

--- voting/urls.py
+++ voting/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name="index"),    
     path('vote', views.votingNow, name="vote"),
-    path('test', views.some_view, name="some_view"),
-    path('vote', views.cast_vote, name="cast_vote"),
-    path('check', views.check_vote, name="check_vote"),
+    
+    path('api/vote', views.cast_vote, name="cast_vote"),
+    path('api/check', views.check_vote, name="check_vote"),
     
 ]
