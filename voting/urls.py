7147e594f3cc8c7128395ccfd4dd14e417f1841e
George Giamouridis <giamouridis22@gmail.com>
2021-01-04 13:33:56 +0000

    vote path

--- voting/urls.py
+++ voting/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('voting', views.votingNow, name="vote"),
+    path('vote', views.votingNow, name="vote"),
     path('test', views.some_view, name="some_view"),
     path('vote', views.cast_vote, name="cast_vote"),
     path('check', views.check_vote, name="check_vote"),
