b18a918f102c38aa94e11cd56b1027bcb454a6ed
Srikanth <srikanthtk99@gmail.com>
2021-01-07 19:59:46 +0530

    beatified admin login

--- blockvote/urls.py
+++ blockvote/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include, url
 from django.urls import path, include
+from voting.admin import site
+admin.site = site
+admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('voting.urls')),
+    #path('admin/login/', views.home),
 ]
